57555519131c4859007b680a41c2685f3097f437
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-10-06 22:18:58 +0200

    Empezando con el diseño para versión MAUI (mobile)

---
+++ gsCrearClasesTablas_MAUI/MainPage_ori.xaml.cs
@@ -0,0 +1,24 @@
+namespace gsCrearClasesTablas_MAUI
+{
+    public partial class MainPage_ori : ContentPage
+    {
+        int count = 0;
+
+        public MainPage_ori()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
